bc8cd7327e9c2129d7d94440755503fcf75e1450
kenhsieh <kenhsieh@users.noreply.github.com>
2026-06-22 14:54:14 +0800

    Publish Ken bazi dashboard

---
+++ ken-bazi-app/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'ken-bazi-v6.0';
+const ASSETS = [
+  '/ken-bazi-app/',
+  '/ken-bazi-app/index.html',
+  '/ken-bazi-app/icon-192.png',
+  '/ken-bazi-app/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@400;700;900&family=Noto+Sans+TC:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
